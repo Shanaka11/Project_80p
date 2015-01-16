18497666efd3db1ebe1985b2d0c7004b27ebc2c7
Shanaka Abeysinghe <shanakaabeysinghe@gmail.com>
2015-01-16 21:21:37 +0530

    Finished basic part of Course_Node class

--- Project_80p/src/Course_Node.java
+++ Project_80p/src/Course_Node.java
@@ -7,6 +7,48 @@ public class Course_Node {
 	//Variables for storing lectures that were attended and held and the percentage of the attendance
 	int lecs_attend,lecs_held,attend_perc;
 	
+	//Default Constructor sets attendance values at 0
+	Course_Node(){
+		
+		this.lecs_attend = 0;
+		this.lecs_held = 0;
+		this.attend_perc = 0;
+		
+	}
 	
+	// for testing
+	
+	public void print_node(){
+		
+		System.out.println("At: " + this.lecs_attend + " He: " + this.lecs_held + " Pe: "
+				+ this.attend_perc + "%");
+		
+	}
+	
+	// to increment attendance when attended the lecture
+	
+	public void attend_lec(){
+	
+		this.lecs_attend++;
+		this.lecs_held++;
+		percentAge();
+		
+	}
+	
+	// when bunking the lectures
+	
+	public void not_attend_lec(){
+		
+		this.lecs_held++;
+		percentAge();
+		
+	}
 
+	// calculate percentage
+	
+	public void percentAge(){
+		
+		this.attend_perc = this.lecs_attend/this.lecs_held;
+		
+	}
 }
