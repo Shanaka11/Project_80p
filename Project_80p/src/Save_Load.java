4da682de557b11c31dd7d84ef9e8730ecef46421
Shanaka Abeysinghe <shanakaabeysinghe@gmail.com>
2015-01-16 22:07:07 +0530

    Added an static ArrayList to store data and a method to create a file

--- Project_80p/src/Save_Load.java
+++ Project_80p/src/Save_Load.java
@@ -1,6 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class Save_Load {
 	//All file related things 
 	//Creating Reading Saving files
+	
+	// To store File info
+	static ArrayList<Course_Node> Course_List = new ArrayList<Course_Node>();
+	
+	// To Create 
+	private void create_file(String name){
+		
+		File file = new File(name);
+		try {
+			
+			file.createNewFile();
+			
+		} catch (IOException e) {
+			//add a message box to appear to handle this excetion 
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
 
 }
