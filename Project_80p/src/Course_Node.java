65cf781854f2ed18cd1c7e9b3ed200af3460ec4e
Shanaka Abeysinghe <shanakaabeysinghe@gmail.com>
2015-01-16 21:12:44 +0530

    Added Variables for class Cource_Node

--- Project_80p/src/Course_Node.java
+++ Project_80p/src/Course_Node.java
@@ -1,4 +1,12 @@
 
 public class Course_Node {
+	
+	//This class will be used to store info about a single course
+	//Will have methods to calculate percentages and attendance
+	
+	//Variables for storing lectures that were attended and held and the percentage of the attendance
+	int lecs_attend,lecs_held,attend_perc;
+	
+	
 
 }
